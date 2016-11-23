185f178c4a9f8da8f411b4f0acedcf00032dc37f
Marshall Glanzer <mglanzer@grubhub.com>
2016-11-23 13:10:32 -0600

    Initial commit

---
+++ src/main/java/com/glanzer/MgApiApplication.java
@@ -0,0 +1,12 @@
+package com.glanzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgApiApplication.class, args);
+	}
+}
